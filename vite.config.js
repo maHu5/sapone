55521512c5261d53c37bd202ce98d48a264b338d
Mahmut Keygubatli <mahmut.keygubatli@areaway.de>
2025-12-03 16:15:18 +0300

    add github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     base: './',
+    build: {
+        outDir: './build'
+    },
     server: {
         allowedHosts: true,
         host: '0.0.0.0',
